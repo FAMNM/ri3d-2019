3da9aa1b790b73e0bfa027f5a9cf7cd37be96411
Paul Bunyan <hal7df@gmail.com>
2019-01-07 18:47:15 -0500

    drivetrain: add drive inversion

--- src/main/cpp/subsystems/Drivetrain.cpp
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -2,6 +2,8 @@
 #include "famnm/Robot.h"
 #include <iostream>
 
+using namespace famnm;
+
 Drivetrain::Drivetrain ()
     : m_lfDrive(RobotMap::kLeftFrontDrive),
       m_rfDrive(RobotMap::kRightFrontDrive),
@@ -9,15 +11,28 @@ Drivetrain::Drivetrain ()
       m_rrDrive(RobotMap::kRightRearDrive),
       m_lDrive(m_lfDrive, m_lrDrive),
       m_rDrive(m_rfDrive, m_rrDrive),
-      m_drive(m_lDrive, m_rDrive) {}
+      m_drive(m_lDrive, m_rDrive),
+      m_driveMplier(1.) {}
 
 void Drivetrain::teleopDrive () {
-    m_drive.ArcadeDrive(m_driver->readAxis(famnm::XboxAxis::kLeftY),
-                        -m_driver->readAxis(famnm::XboxAxis::kRightX));
+    m_drive.ArcadeDrive((m_driveMplier * m_driver->readAxis(XboxAxis::kLeftY)),
+                        (-m_driveMplier * m_driver->readAxis(XboxAxis::kRightX)));
 }
 
 void Drivetrain::init () {
     m_driver = &getParent()->getGamepad(RobotMap::kDriver);
+
+    auto toggleReverse = [this] () {
+        if (!(m_driver->readButton(XboxButton::kStart)
+                && m_driver->readButton(XboxButton::kBack))) {
+            return;
+        }
+
+        m_driveMplier = -m_driveMplier;
+    }
+
+    m_driver->bind(XboxButton::kStart, Gamepad::kDown, toggleReverse);
+    m_driver->bind(XboxButton::kBack, Gamepad::kDown, toggleReverse);
 }
 
 void Drivetrain::teleop () {
